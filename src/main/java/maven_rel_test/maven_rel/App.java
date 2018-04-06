1e7ae8852dcc52bcd91d0abdeff3d226e837092c
Sakith <gmsakith@gmail.com>
2018-04-06 14:41:17 +0530

    new changes

---
+++ src/main/java/maven_rel_test/maven_rel/App.java
@@ -0,0 +1,13 @@
+package maven_rel_test.maven_rel;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
