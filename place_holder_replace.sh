c3c47d0abc2d0ea9c8c46e34bf7779bb6e38053f
Sakith <gmsakith@gmail.com>
2018-04-13 23:01:33 +0530

    remove unwanted content

--- place_holder_replace.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-ex -sc '%s/{REPOSITORY_USER_NAME}/'"$REPOSITORY_USER_NAME"'/g' -cx settings.xml
-ex -sc '%s/{JFROG_USERNAME}/'"$JFROG_USERNAME"'/g' -cx settings.xml
-ex -sc '%s/{REPOSITORY_PASSWORD}/'"$REPOSITORY_PASSWORD"'/g' -cx settings.xml
-ex -sc '%s/{JFROG_KEY}/'"$JFROG_KEY"'/g' -cx settings.xml
-
-ex -sc '%s/{REPOSITORY_USER_NAME}/'"$REPOSITORY_USER_NAME"'/g' -cx release.py
-ex -sc '%s/{REPOSITORY_PASSWORD}/'"$REPOSITORY_PASSWORD"'/g' -cx release.py
